d6a1aa2c733b8c1355b7e0f9dba4ef9b961f7d6b
franco-c <tjrdnjs33936@gmail.com>
2013-04-13 20:04:45 +0900

    Follow xy's build.prop tweaks

--- config/mirage.mk
+++ config/mirage.mk
@@ -5,11 +5,15 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_VERSION_MAJOR = 4.2.2
 PRODUCT_VERSION_MINOR = STABLE
-PRODUCT_VERSION_MAINTENANCE = 1.15
+PRODUCT_VERSION_MAINTENANCE = 1.2
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.miracle.version=MIRAGE-$(PRODUCT_RELEASE_NAME)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).v$(PRODUCT_VERSION_MAINTENANCE) \
     ro.mirage.version=MIRAGE-v$(PRODUCT_VERSION_MAINTENANCE)-$(PRODUCT_VERSION_MINOR) \
-    video.accelerate.hw=1 \
-    ro.media.enc.jpeg.quality=100
+    debug.composition.type=gpu \
+    debug.sf.hw=1 \
+    ro.kernel.android.checkjni=0 \
+    ro.kernel.checkjni=0 \
+    ro.media.enc.jpeg.quality=100 \
+    persist.sys.root_access=3
 
