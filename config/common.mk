c505da7b40cc73eb678c6487ecf47d9a97788b00
franco-c <tjrdnjs33936@gmail.com>
2013-04-23 17:33:34 +0900

    Separate audio and push to 4.2

--- config/common.mk
+++ config/common.mk
@@ -29,53 +29,8 @@ PRODUCT_COPY_FILES += \
     vendor/mirage/prebuilt/common/bin/compcache:system/bin/compcache \
     vendor/mirage/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
 
-# Audio Config for DSPManager
-PRODUCT_COPY_FILES += \
-    vendor/mirage/prebuilt/common/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
-
-# Bring in media files
-PRODUCT_COPY_FILES +=  \
-    vendor/mirage/prebuilt/common/media/audio/alarms/Alarm_Classic.ogg:system/media/audio/alarms/Alarm_Classic.ogg \
-    vendor/mirage/prebuilt/common/media/audio/alarms/Alarm_Rooster_02.ogg:system/media/audio/alarms/Alarm_Rooster_02.ogg \
-    vendor/mirage/prebuilt/common/media/audio/alarms/Cesium.ogg:system/media/audio/alarms/Cesium.ogg \
-    vendor/mirage/prebuilt/common/media/audio/alarms/Alarm_Beep_02.ogg:system/media/audio/alarms/Alarm_Beep_02.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/NewPlayer.ogg:system/media/audio/ringtones/NewPlayer.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/Ring_Digital_02.ogg:system/media/audio/ringtones/Ring_Digital.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/Playa.ogg:system/media/audio/ringtones/Playa.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/Pyxis.ogg:system/media/audio/ringtones/Pyxis.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/Vespa.ogg:system/media/audio/ringtones/Vespa.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/hydra.ogg:system/media/audio/ringtones/hydra.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/Aquila.ogg:system/media/audio/ringtones/Aquila.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/World.ogg:system/media/audio/ringtones/World.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/InsertCoin.ogg:system/media/audio/ringtones/InsertCoin.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ringtones/Carina.ogg:system/media/audio/ringtones/Carina.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Antares.ogg:system/media/audio/notifications/Antares.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Doink.ogg:system/media/audio/notifications/Doink.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/arcturus.ogg:system/media/audio/notifications/arcturus.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Plastic_Pipe.ogg:system/media/audio/notifications/Plastic_Pipe.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Procyon.ogg:system/media/audio/notifications/Procyon.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/pixiedust.ogg:system/media/audio/notifications/pixiedust.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/DontPanic.ogg:system/media/audio/notifications/DontPanic.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Palladium.ogg:system/media/audio/notifications/Palladium.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/tweeters.ogg:system/media/audio/notifications/tweeters.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Cricket.ogg:system/media/audio/notifications/Cricket.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Altair.ogg:system/media/audio/notifications/Altair.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Helium.ogg:system/media/audio/notifications/Helium.ogg \
-    vendor/mirage/prebuilt/common/media/audio/notifications/Aldebaran.ogg:system/media/audio/notifications/Aldebaran.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/Undock.ogg:system/media/audio/ui/Undock.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/Dock.ogg:system/media/audio/ui/Dock.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
-    vendor/mirage/prebuilt/common/media/audio/ui/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg
-#LOCAL SLIM CHANGES  - END
+# Common Audio mk
+include vendor/mirage/config/audio_common.mk
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -137,7 +92,6 @@ PRODUCT_PACKAGES += \
 
 # Extra tools
 PRODUCT_PACKAGES += \
-    openvpn \
     e2fsck \
     mke2fs \
     tune2fs
@@ -149,10 +103,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/mirage/overlay/common
 include vendor/mirage/config/themes_common.mk
 
 # Versioning System
-# Reopen for 4.2.2.build.4.1
+# Reopen for 4.2.2.build.4.2
 PRODUCT_VERSION_MAJOR = 4.2.2
 PRODUCT_VERSION_MINOR = build
-PRODUCT_VERSION_MAINTENANCE = 4.1
+PRODUCT_VERSION_MAINTENANCE = 4.2
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(BUILD_ID)
 
