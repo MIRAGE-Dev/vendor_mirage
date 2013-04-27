fb259ef6f0dcef046edb4305f7bbe1a9f0ab99f8
franco-c <tjrdnjs33936@gmail.com>
2013-04-27 19:19:39 +0900

    Mirage v1.5: Numb

--- config/mirage.mk
+++ config/mirage.mk
@@ -4,8 +4,13 @@ PRODUCT_COPY_FILES += \
     vendor/mirage/proprietary/KoreanIME/libjni_koreanime.so:system/lib/libjni_koreanime.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.execution-mode=int:jit \
     debug.composition.type=gpu \
+    debug.performance.tuning=1 \
     debug.sf.hw=1 \
+    pm.sleep_mode=1 \
+    video.accelerate.hw=1 \
     ro.kernel.android.checkjni=0 \
     ro.media.enc.jpeg.quality=100 \
     persist.sys.root_access=3
