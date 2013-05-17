8aabf5cbba44aa18ee7062e529ff94224f0e2ebb
franco-c <tjrdnjs33936@gmail.com>
2013-05-17 11:38:50 +0900

    Mirage v2.2: Mirage

--- config/mirage.mk
+++ config/mirage.mk
@@ -6,10 +6,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-flags=m=y \
     dalvik.vm.execution-mode=int:jit \
-    debug.composition.type=gpu \
-    debug.performance.tuning=1 \
     pm.sleep_mode=1 \
-    video.accelerate.hw=1 \
     ro.kernel.android.checkjni=0 \
     ro.media.enc.jpeg.quality=100 \
     persist.sys.root_access=3
